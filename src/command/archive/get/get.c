81abcf4a08318e7a8153077c4f28255000a30f13
Denis Smirnov <sd@arenadata.io>
2021-04-15 15:41:18 +0300

    Typo fixes

--- src/command/archive/get/get.c
+++ src/command/archive/get/get.c
@@ -416,7 +416,7 @@ archiveGetCheck(const StringList *archiveRequestList)
                     InfoPgData pgData = infoPgData(infoArchivePg(info), pgIdx);
 
                     // Only use the archive id if it matches the current cluster
-                    bool version_check = controlInfo.version >= GP_VERSION_7 ? 
+                    bool version_check = controlInfo.version >= GP_VERSION_7 ?
                                          MAJOR_VERSION(pgData.version) == MAJOR_VERSION(controlInfo.version) :
                                          pgData.version == controlInfo.version;
                     if (pgData.systemId == controlInfo.systemId && version_check)
