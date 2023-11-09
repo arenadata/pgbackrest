c92e53a6e7d9404a0add424263c0710f2ceda92b
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-11-09 09:59:34 +0500

    fix

--- src/common/io/read.c
+++ src/common/io/read.c
@@ -134,8 +134,9 @@ ioReadInternal(IoRead *this, Buffer *buffer, bool block)
                     if (ioReadBlock(this) && bufRemains(this->input) > bufRemains(buffer))
                         bufLimitSet(this->input, bufRemains(buffer));
 
-                    this->pub.interface.size += this->pub.interface.read(this->pub.driver, this->input, block);
+                    this->pub.interface.read(this->pub.driver, this->input, block);
                     bufLimitClear(this->input);
+                    this->pub.interface.size += bufUsed(this->input);
                 }
                 // Set input to NULL and flush (no need to actually free the buffer here as it will be freed with the mem context)
                 else
