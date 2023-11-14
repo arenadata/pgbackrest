abb399938191890614dc398b7633d80227f4f2a6
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-11-14 15:00:46 +0500

    optimnize and comment

--- src/command/backup/file.c
+++ src/command/backup/file.c
@@ -293,8 +293,8 @@ backupFile(
                         do
                         {
                             ioRead(storageReadIo(read), buffer);
-                            // open destination and copy the file only if read something or not bundling
-                            if (ioReadInterface(storageReadIo(read))->totalBytesRead > 0 || bundleId == 0)
+                            // Copy the file in bundling mode or if the file size is not zero
+                            if (bundleId == 0 || ioReadInterface(storageReadIo(read))->totalBytesRead != 0)
                             {
                                 // Setup the repo file for write. There is no need to write the file atomically (e.g.
                                 // via a temp file on Posix) because checksums are tested on resume after a failed
