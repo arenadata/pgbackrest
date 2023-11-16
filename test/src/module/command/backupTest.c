6c10de7d161a0fff2053a90833b28f7975e6e4e0
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-11-16 13:37:39 +0500

    fix comment

--- test/src/module/command/backupTest.c
+++ test/src/module/command/backupTest.c
@@ -1688,7 +1688,8 @@ testRun(void)
         TEST_RESULT_UINT(result.copySize, 0, "copy size is 0");
         TEST_RESULT_UINT(result.backupCopyResult, backupCopyResultTruncate, "truncated file");
         TEST_RESULT_PTR(result.copyChecksum, HASH_TYPE_SHA1_ZERO_BUF, "checksum eq");
-        TEST_STORAGE_EXISTS(storageRepo(), strZ(repoFile), .not_exists = true, .comment = "bundlefile is not put to storage");
+        TEST_STORAGE_EXISTS(storageRepo(), strZ(repoFile),
+            .not_exists = true, .comment = "truncated file is not added to bundle, bundle is not created");
 
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("copy file to encrypted repo");
