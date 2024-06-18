3a980a77cbec39be2c45ea097e12e45c5c8a7ea4
Alexander Kondakov <a.kondakov@arenadata.io>
2024-06-18 11:12:28 +0300

    Non empty GPDB_IMAGE arg

--- test/gpdb/run_docker.sh
+++ test/gpdb/run_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-DOCKERIMAGE="$1"
+DOCKERIMAGE="${1:-gpdb6_regress:latest}"
 SCRIPT_PATH="$(dirname "$(realpath -s "$0")")"
 PROJECT_ROOT="$(dirname "$(dirname "$SCRIPT_PATH")")"
 
