8921b8c835d3a8daed2e0c5102c506275de83f4c
Viktor Kurilko <v.kurilko@arenadata.io>
2024-08-30 18:23:23 +0700

    fix format

--- test/src/module/common/partialRestoreTest.c
+++ test/src/module/common/partialRestoreTest.c
@@ -67,31 +67,37 @@ testRun(void)
         JsonRead *json = jsonReadNew(jsonstr);
         List *filterList = buildFilterList(json);
         TEST_RESULT_UINT(lstSize(filterList), 7, "tables count");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1600, .dbNode = 20000, .relNode = 16384}),
-                             lstGet(filterList, 0), sizeof(RelFileNode)) == 0, true, "Check the 1st element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1600, .dbNode = 20000, .relNode = 16386}),
-                             lstGet(filterList, 1), sizeof(RelFileNode)) == 0, true, "Check the 2nd element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1601, .dbNode = 20000, .relNode = 16385}),
-                             lstGet(filterList, 2), sizeof(RelFileNode)) == 0, true, "Check the 3rd element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1700, .dbNode = 20001, .relNode = 11000}),
-                             lstGet(filterList, 3), sizeof(RelFileNode)) == 0, true, "Check the 4th element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1700, .dbNode = 20001, .relNode = 16386}),
-                             lstGet(filterList, 4), sizeof(RelFileNode)) == 0, true, "Check the 5th element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 1701, .dbNode = 20001, .relNode = 10000}),
-                             lstGet(filterList, 5), sizeof(RelFileNode)) == 0, true, "Check the 6th element");
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 0, .dbNode = 20002, .relNode = 0}),
-                             lstGet(filterList, 6), sizeof(RelFileNode)) == 0, true, "Check the 7th element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1600, .dbNode = 20000, .relNode = 16384}), lstGet(filterList, 0), sizeof(RelFileNode)),
+                0, "Check the 1st element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1600, .dbNode = 20000, .relNode = 16386}), lstGet(filterList, 1), sizeof(RelFileNode)),
+                0, "Check the 2nd element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1601, .dbNode = 20000, .relNode = 16385}), lstGet(filterList, 2), sizeof(RelFileNode)),
+                0, "Check the 3rd element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1700, .dbNode = 20001, .relNode = 11000}), lstGet(filterList, 3), sizeof(RelFileNode)),
+                0, "Check the 4th element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1700, .dbNode = 20001, .relNode = 16386}), lstGet(filterList, 4), sizeof(RelFileNode)),
+                0, "Check the 5th element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 1701, .dbNode = 20001, .relNode = 10000}), lstGet(filterList, 5), sizeof(RelFileNode)),
+                0, "Check the 6th element");
+        TEST_RESULT_INT(
+            memcmp(
+                &((RelFileNode){.spcNode = 0, .dbNode = 20002, .relNode = 0}), lstGet(filterList, 6), sizeof(RelFileNode)),
+                0, "Check the 7th element");
         // This is necessary to cover the comparator
         RelFileNode *found = lstFind(filterList, &((RelFileNode){.spcNode = 0, .dbNode = 20002, .relNode = 0}));
-        TEST_RESULT_BOOL(memcmp(
-                             &((RelFileNode){.spcNode = 0, .dbNode = 20002, .relNode = 0}), found,
-                             sizeof(RelFileNode)) == 0, true, "test find");
+        TEST_RESULT_INT(
+            memcmp(&((RelFileNode){.spcNode = 0, .dbNode = 20002, .relNode = 0}), found, sizeof(RelFileNode)), 0, "test find");
     }
 }
