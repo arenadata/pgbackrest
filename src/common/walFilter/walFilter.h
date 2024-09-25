79d1f80838ff640b4524cd5f5c724374d25caa62
Viktor Kurilko <v.kurilko@arenadata.io>
2024-09-25 14:33:59 +0700

    Remove unnecessary include

--- src/common/walFilter/walFilter.h
+++ src/common/walFilter/walFilter.h
@@ -2,9 +2,7 @@
 #define COMMON_WALFILTER_WALFILTER_H
 
 #include "command/archive/get/file.h"
-#include "common/io/filter/filter.h"
-#include "common/type/json.h"
-#include "postgres/interface/static.vendor.h"
+#include "postgres/interface.h"
 
 #define WAL_FILTER_TYPE                                   STRID5("wal-fltr", 0x95186db0370)
 
