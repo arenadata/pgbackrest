57108b7c42e6409fc95d9d24caaec011a79a9861
David Steele <david@pgbackrest.org>
2024-10-08 18:15:34 +0300

    Shuffle PostgreSQL versions between test containers.
    Ubuntu 20.04 has been having consistent errors starting PostgreSQL 10 so move 9.5 to this container instead. An older version makes sense with an older distro.
    
    Also move PostgreSQL 12 from RHEL 8 since this version will be EOL soon.

--- test/lib/pgBackRestTest/Common/VmTest.pm
+++ test/lib/pgBackRestTest/Common/VmTest.pm
@@ -159,9 +159,9 @@ my $oyVm =
 
         &VM_DB_TEST =>
         [
-            PG_VERSION_12,
             PG_VERSION_14,
             PG_VERSION_15,
+            PG_VERSION_16,
         ],
     },
 
@@ -221,8 +221,8 @@ my $oyVm =
         &VM_DB_TEST =>
         [
             PG_VERSION_94,
+            PG_VERSION_95,
             PG_VERSION_96,
-            PG_VERSION_10,
         ],
     },
 
@@ -254,9 +254,9 @@ my $oyVm =
 
         &VM_DB_TEST =>
         [
-            PG_VERSION_95,
+            PG_VERSION_10,
             PG_VERSION_11,
-            PG_VERSION_16,
+            PG_VERSION_12,
             PG_VERSION_17,
         ],
     },
