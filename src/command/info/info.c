6c1ff2ddbefdb44110949005ea7d5e9e9d30d028
Maksim Michkov <m.michkov@arenadata.io>
2025-01-17 10:22:52 +0000

    Fix

--- src/command/info/info.c
+++ src/command/info/info.c
@@ -1699,11 +1699,12 @@ infoRender(void)
                         if (backupLockHeld || restoreLockHeld)
                         {
                             strCatFmt(
-                                resultStr, "%s (%s%s\n", INFO_STANZA_STATUS_OK,
+                                resultStr, "%s (%s%s%s\n", INFO_STANZA_STATUS_OK,
                                 backupLockHeld == true ?
                                     zNewFmt(INFO_STANZA_STATUS_MESSAGE_LOCK_BACKUP "%s", strZ(backupPercentCompleteStr)) : "",
+                                backupLockHeld && restoreLockHeld ? ", " : "",
                                 restoreLockHeld == true ?
-                                    zNewFmt(", " INFO_STANZA_STATUS_MESSAGE_LOCK_RESTORE "%s)", strZ(restorePercentCompleteStr)) : ")");
+                                    zNewFmt(INFO_STANZA_STATUS_MESSAGE_LOCK_RESTORE "%s)", strZ(restorePercentCompleteStr)) : ")");
                         }
                         else
                             strCatFmt(resultStr, "%s\n", INFO_STANZA_STATUS_OK);
