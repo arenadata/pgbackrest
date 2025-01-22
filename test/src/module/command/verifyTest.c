6b28367c982ca78aee98f769b9a879f38a95fbf1
Maksim Michkov <m.michkov@arenadata.io>
2025-01-22 12:46:40 +0000

    Fix --set test

--- test/src/module/command/verifyTest.c
+++ test/src/module/command/verifyTest.c
@@ -2066,7 +2066,7 @@ testRun(void)
             "status: error\n"
             "  archiveId: none found\n"
             "  backup: 20181119-152900F_20181119-152909D, status: invalid, total files checked: 1, total valid files: 0\n"
-            "    missing: 0, checksum invalid: 1, size invalid: 0, other: 0", "--set with a valid backup label\n");
+            "    missing: 0, checksum invalid: 1, size invalid: 0, wal invalid: 0, other: 0", "--set with a valid backup label\n");
         TEST_RESULT_LOG(
             "P00 DETAIL: no archives exist in the repo\n"
             "P01   INFO: invalid checksum '20181119-152900F/pg_data/PG_VERSION'");
