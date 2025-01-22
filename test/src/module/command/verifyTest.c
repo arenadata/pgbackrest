6c59c49544298db153332a9ef718311441bd3caa
Maksim Michkov <m.michkov@arenadata.io>
2025-01-22 12:15:40 +0000

    Fix test

--- test/src/module/command/verifyTest.c
+++ test/src/module/command/verifyTest.c
@@ -400,7 +400,6 @@ testRun(void)
         List *archiveIdResultList = lstNewP(sizeof(VerifyArchiveResult), .comparator = archiveIdComparator);
         lstAdd(archiveIdResultList, &archiveResult);
         VerifyArchiveResult *archiveIdResult = lstGetLast(archiveIdResultList);
-        List *backupList = lstNewP(sizeof(VerifyBackupResult));
 
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("Single WAL");
