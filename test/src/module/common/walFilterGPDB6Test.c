144de5e5ac7a5f1d6b8b8a7eb36abbb4a5b44428
Viktor Kurilko <v.kurilko@arenadata.io>
2025-07-29 15:56:44 +0700

    test

--- test/src/module/common/walFilterGPDB6Test.c
+++ test/src/module/common/walFilterGPDB6Test.c
@@ -1274,7 +1274,7 @@ testRun(void)
             STORAGE_REPO_ARCHIVE "/9.4-1/0000000100000000/000000010000000000000002-abcdabcdabcdabcdabcdabcdabcdabcdabcdabcd");
         MEM_CONTEXT_TEMP_END();
 
-        TEST_TITLE("Unexpected WAL end");
+        TEST_TITLE("missing next file");
         MEM_CONTEXT_TEMP_BEGIN();
         filter = walFilterNew(pgControl, &archiveInfo);
         {
