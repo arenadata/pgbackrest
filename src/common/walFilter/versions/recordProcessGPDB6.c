069c4ebc6a561fead16eff2f703e0dfa67ff9ca9
Viktor Kurilko <v.kurilko@arenadata.io>
2025-08-04 14:49:20 +0700

    Add support for orphaned files WAL record
    Add support for two new types of WAL records: XLOG_PENDING_DELETE and
    XLOG_SMGR_CREATE_PDL. XLOG_SMGR_CREATE_PDL records are processed in the same way
    as XLOG_SMGR_CREATE. XLOG_PENDING_DELETE records are filtered out.

--- src/common/walFilter/versions/recordProcessGPDB6.c
+++ src/common/walFilter/versions/recordProcessGPDB6.c
@@ -54,6 +54,7 @@ getXlog(const XLogRecordGPDB6 *record)
         case XLOG_END_OF_RECOVERY:
         case XLOG_OVERWRITE_CONTRECORD:
         case XLOG_SWITCH:
+        case XLOG_PENDING_DELETE:
 //          ignore
             return NULL;
 
@@ -72,6 +73,7 @@ getStorage(const XLogRecordGPDB6 *record)
     switch (info)
     {
         case XLOG_SMGR_CREATE:
+        case XLOG_SMGR_CREATE_PDL:
             return (const RelFileNode *) XLogRecGetData(record);
 
         case XLOG_SMGR_TRUNCATE:
@@ -459,7 +461,8 @@ filterRecordGPDB6(XLogRecordBase *const recordBase)
 
     const RelFileNode *const node = getRelFileNode(record);
 
-    if (node == NULL || isRelationNeeded(node->dbNode, node->spcNode, node->relNode))
+    if ((node == NULL || isRelationNeeded(node->dbNode, node->spcNode, node->relNode)) &&
+        !(record->xl_rmid == RM_XLOG_ID && (record->xl_info & ~XLR_INFO_MASK) == XLOG_PENDING_DELETE))
     {
         return;
     }
